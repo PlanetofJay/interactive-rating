3ecf894bde6afcaf3ea8f594ec961ee2b3fc0a49
PlanetofJay <jae44dv@gmail.com>
2023-03-13 18:39:14 -0400

    add botton states

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import styles from "./Rating.module.css";
 
 export function Rating() {
 
+  // Rating buttons
+  const [selectedRating, setSelectedRating] = useState<number>();
+  // Submit button
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function handleRatingClicked(rating: number) {
+    setSelectedRating(rating);
+  }
+
+  function handleFormSubmitted(e: React.FormEvent) {
+    e.preventDefault();
+    setIsSubmitted(true);
+  }
+
   return (
 
-    <div className={styles.panel}>
-
+    <form onSubmit={handleFormSubmitted} className={styles.panel}>
       <img className={styles.star} src="/icon-star.svg"></img>
 
       <h1 className={styles.title}>How did we do?</h1>
@@ -16,15 +30,20 @@ export function Rating() {
       </p>
 
       <div className={styles.group}>
-        <button className={styles.rating}>1</button>
-        <button className={styles.rating}>2</button>
-        <button className={styles.rating}>3</button>
-        <button className={styles.rating}>4</button>
-        <button className={styles.rating}>5</button>
+        {[1, 2, 3, 4, 5].map((rating) => (
+          <button 
+          type="button"
+          onClick={() => handleRatingClicked(rating)} 
+          className={styles.rating}>
+            {rating}
+          </button>
+        ))}
       </div>
 
-      <button className={styles.submit}>Submit</button>
-    </div>
+      <button disabled={selectedRating === undefined} className={styles.submit}>
+        Submit
+      </button>
+    </form>
 
   );
 
