9c7606e6a8428f67e4d58c47383654a3102b30f2
PlanetofJay <jae44dv@gmail.com>
2023-03-13 20:09:53 -0400

    complete

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -17,24 +17,36 @@ export function Rating() {
     setIsSubmitted(true);
   }
 
-  return (
+  return isSubmitted ? (
+    <div className={styles["thank-you-panel"]}>
+      <img src="/illustration-thank-you.svg"></img>
 
+      <p className={styles.selected}>You selected {selectedRating} out of 5</p>
+
+      <h1 className={styles.title}>Thank you!</h1>
+
+      <p className={styles.description}>
+        We appreciate you taking the time to give a rating. If you ever need 
+        more support, don't hesitate to get in touch!
+      </p>
+    </div>
+  ) : (
     <form onSubmit={handleFormSubmitted} className={styles.panel}>
       <img className={styles.star} src="/icon-star.svg"></img>
 
       <h1 className={styles.title}>How did we do?</h1>
 
       <p className={styles.description}>
-        Please let us know how we did with your support request. All feedback is 
+        Please let us know how we did with your support request. All feedback is
         appreciated to help us improve our offering!
       </p>
 
       <div className={styles.group}>
         {[1, 2, 3, 4, 5].map((rating) => (
-          <button 
-          type="button"
-          onClick={() => handleRatingClicked(rating)} 
-          className={styles.rating}>
+          <button
+            type="button"
+            onClick={() => handleRatingClicked(rating)}
+            className={styles.rating}>
             {rating}
           </button>
         ))}
