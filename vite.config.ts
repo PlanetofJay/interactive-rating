4135264d87aa2eb19c5391fcb935e832fbfaa8fe
PlanetofJay <jae44dv@gmail.com>
2023-03-28 17:37:31 -0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default {
-  base: "https://planetofjay.github.io/interactive-rating"
-}
+export default defineConfig({
+  base: "https://planetofjay.github.io/interactive-rating",
+  plugins: [react()],
+})
